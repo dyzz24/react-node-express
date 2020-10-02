5d4e3ed77cb959dc656d89dfdea89a1591cf399e
DVasilkov <dvasilkov@stream.ru>
2020-10-02 19:37:06 +0300

    useRouterMatch

--- frontend/src/components/main/forum-page/index.tsx
+++ frontend/src/components/main/forum-page/index.tsx
@@ -1,3 +1,9 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router';
 
-export const ForumPage = () => <div>i am forum page</div>;
+export const ForumPage = () => {
+  const location = useRouteMatch();
+  const { themeId } = location.params as { themeId: string };
+  console.log(themeId);
+  return <div>i am forum page {themeId}</div>;
+};
