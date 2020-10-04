86851d939f54ad3bb4d701ef8d699e553bd7b004
DVasilkov <dvasilkov@stream.ru>
2020-10-04 16:08:28 +0300

    input (dirty)

--- frontend/src/components/main/new-message/index.tsx
+++ frontend/src/components/main/new-message/index.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 
 export const NewMessage = () => {
   const [message, setMessage] = useState({ title: '', text: '' });
+
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>, key: string) => {
+    e.persist();
+    setMessage((state) => ({ ...state, [key]: e.target.value }));
+  };
   return (
     <form>
-      <input />
-      <input />
+      <input
+        value={message.title}
+        onChange={(event) => onInputChange(event, 'title')}
+      />
+      <input
+        value={message.text}
+        onChange={(event) => onInputChange(event, 'text')}
+      />
       <button type={'submit'} />
     </form>
   );
